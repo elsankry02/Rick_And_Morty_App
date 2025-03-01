045491541a03d66b6907b6d9d03112acba24b68c
elsankry02 <githup800900@gmail.com>
2025-03-01 15:52:33 +0200

    edit part 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:rick_and_morty/app.dart';
 
 void main() {
-  runApp(const RickAndMorty());
+  runApp(const ProviderScope(child: RickAndMorty()));
 }
--- lib/pages/character/character_page.dart
+++ lib/pages/character/character_page.dart
@@ -1,107 +1,79 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:rick_and_morty/business_logic/cubit/character_cubit.dart';
 import 'package:rick_and_morty/core/constant/color_manger.dart';
-import 'package:rick_and_morty/data/web_services/character_api.dart';
 import 'package:rick_and_morty/pages/character/widget/details_character_widget.dart';
+import 'package:rick_and_morty/providers/rick_and_morty_provider.dart/rick_and_morty_provider.dart';
 
 @RoutePage()
-class CharacterPage extends StatefulWidget {
+class CharacterPage extends ConsumerStatefulWidget {
   const CharacterPage({super.key});
 
   @override
-  State<CharacterPage> createState() => _CharacterPageState();
+  ConsumerState<CharacterPage> createState() => _CharacterPageState();
 }
 
-class _CharacterPageState extends State<CharacterPage> {
+class _CharacterPageState extends ConsumerState<CharacterPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CharacterCubit>(
-      create: (context) => CharacterCubit(
-        ApiService(Dio()),
-      )..fetchAllCharacter(),
-      child: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: ColorManger.yellow,
-          title: const Text(
-            'Rick and Morty',
-            style: TextStyle(
-              color: ColorManger.grey,
-              fontWeight: FontWeight.bold,
-            ),
+    final futurprovider = ref.read(rickAndMortyProvider);
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: ColorManger.yellow,
+        title: const Text(
+          'Rick and Morty',
+          style: TextStyle(
+            color: ColorManger.grey,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: ColorManger.grey,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocConsumer<CharacterCubit, CharacterState>(
-              listener: (context, state) {
-                if (state is CharactersFailuer) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('bad response'),
+      ),
+      backgroundColor: ColorManger.grey,
+      body: futurprovider.when(
+        data: (data) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                  child: GridView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      childAspectRatio: 2 / 3,
                     ),
-                  );
-                }
-                if (state is CharactersSuccess) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('success'),
+                    itemCount: data.length,
+                    itemBuilder: (context, index) => DetailsCharacterWidget(
+                      results: data[index],
                     ),
-                  );
-                }
-              },
-              builder: (context, state) {
-                //! Success
-                if (state is CharactersSuccess) {
-                  return Expanded(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.only(left: 10, right: 10, top: 10),
-                      child: GridView.builder(
-                        physics: const BouncingScrollPhysics(),
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 10,
-                          mainAxisSpacing: 10,
-                          childAspectRatio: 2 / 3,
-                        ),
-                        itemCount: state.characterModel.length,
-                        itemBuilder: (context, index) => DetailsCharacterWidget(
-                          results: state.characterModel[index],
-                        ),
-                      ),
-                    ),
-                  );
-                }
-                //! Failuer
-                if (state is CharactersFailuer) {
-                  return Center(
-                    child: Text(
-                      state.errorMassage,
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                  );
-                }
-                //! Loading
-                {
-                  return Center(
-                    child: LoadingAnimationWidget.twistingDots(
-                        leftDotColor: ColorManger.white,
-                        rightDotColor: ColorManger.yellow,
-                        size: 200),
-                  );
-                }
-              },
+                  ),
+                ),
+              ),
+            ],
+          );
+        },
+        error: (error, _) {
+          return Center(
+            child: Text(
+              error.toString(),
+              style: const TextStyle(color: Colors.white),
             ),
-          ],
-        ),
+          );
+        },
+        loading: () {
+          return Center(
+            child: LoadingAnimationWidget.twistingDots(
+                leftDotColor: ColorManger.white,
+                rightDotColor: ColorManger.yellow,
+                size: 200),
+          );
+        },
       ),
     );
   }
