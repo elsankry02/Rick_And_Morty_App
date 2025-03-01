b5f939ccc1205c896d066d3ab771762ae19a7ccd
elsankry02 <githup800900@gmail.com>
2025-03-01 15:18:21 +0200

    create FutureProvider rickAndMortyProvider

---
+++ lib/providers/rick_and_morty_provider.dart/rick_and_morty_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:rick_and_morty/data/models/results_model.dart';
+import 'package:rick_and_morty/providers/provider.dart';
+
+final rickAndMortyProvider = FutureProvider<List<Results>>((ref) async {
+  final service = ref.read(resultsServiceProvider);
+  return await service.rickAndMortyFunc();
+});
